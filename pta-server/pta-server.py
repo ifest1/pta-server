ccbf20451068b90ad05165093731e281474777cc
Iago Estrela <hiagoestrelas@gmail.com>
2021-05-09 20:30:20 -0300

    ajustes

--- pta-server/pta-server.py
+++ pta-server/pta-server.py
@@ -3,16 +3,18 @@ import socket
 import sys
 sys.path.append('..')
 
-responses = ['OK', 'NOK', 'ARQ', 'ARQS']
+
 
 class PTAServer:
-    def __init__(self, host='127.0.0.1', port=11550):
+    def __init__(self, host='127.0.0.1', port=11550, files='files'):
         self.host = host
         self.port = port
         self.connections_opened = []
-        self.files_directory = 'files'
+        self.files_directory = files
         self.seq_num = None
         self.users = []
+
+        self.responses = ['OK', 'NOK', 'ARQ', 'ARQS']
 
         self.operations = {
             'PEGA': self.send_file,
@@ -21,23 +23,22 @@ class PTAServer:
             'TERM': self.close_connection
         }
 
+        self.get_users()
+    
+    def get_users(self):
         with open('users.txt', 'r') as f:
             for user in f.readlines():
                 self.users.append(user.rstrip('\n'))
 
     def send_packet(self, conn, payload):
-        try:
-            payload = '{} {}'.format(str(self.seq_num), payload)
-            payload = payload.encode()
-            conn.sendall(payload)
-    
-        except:
-            pass
+        payload = '{} {}'.format(str(self.seq_num), payload)
+        payload = payload.encode()
+        conn.sendall(payload)
 
     def send_listof_files(self, conn):
         if not self.is_connected(conn):
             self.bad_action(conn)
-            return -1
+            return 1
 
         filenames = []
 
@@ -47,45 +48,43 @@ class PTAServer:
         size = len(filenames)
         filenames = ','.join(filenames)
 
-        payload = '{} {} {},'.format(responses[3], str(size), filenames)
+        payload = '{} {} {},'.format(self.responses[3], str(size), filenames)
         
         self.send_packet(conn, payload)
 
         return 1
         
     def send_file(self, conn, filename):
-        if not self.is_connected(conn):
-            self.bad_action(conn)
-            return -1
+        if not self.is_connected(conn): self.bad_action(conn)
+
         try:
             fd = open('./{}/{}'.format(self.files_directory, filename), 'rb')
-        except Exception as e:
-            self.bad_action(conn)
-            return -1
+            raw_file = fd.read()
+            length = len(raw_file)
+            payload = '{} {} {} '.format(self.seq_num, self.responses[2], length)
+            payload = payload.encode() + raw_file
+            conn.sendall(payload)
+            fd.close()
 
-        raw_file = fd.read()
-        length = len(raw_file)
-        payload = '{} {} {} '.format(self.seq_num, responses[2], length)
-        payload = payload.encode() + raw_file
-        conn.sendall(payload)
-        fd.close()
+        except Exception: self.bad_action(conn)
+
         return 1
 
     def open_connection(self, conn, user):
         if user in self.users:
             self.connections_opened.append(conn)
-            self.send_packet(conn, responses[0])
+            self.send_packet(conn, self.responses[0])
             return 1
-        else:
-            self.abort_connection(conn)
-            return 0
+
+        self.abort_connection(conn)
+        return 0
 
     def abort_connection(self, conn):
-        self.send_packet(conn, responses[1])
+        self.send_packet(conn, self.responses[1])
         conn.close()
 
     def close_connection(self, conn):
-        self.send_packet(conn, responses[0])
+        self.send_packet(conn, self.responses[0])
         if self.is_connected(conn):
             idx = self.connections_opened.index(conn)
             del self.connections_opened[idx]
@@ -99,7 +98,7 @@ class PTAServer:
         return user in self.users
 
     def bad_action(self, conn):
-        self.send_packet(conn, responses[1])
+        self.send_packet(conn, self.responses[1])
 
     def splitted_data(self, data):
         data = data.decode()
@@ -125,10 +124,9 @@ class PTAServer:
 
                 self.seq_num = int(data[0])
 
-                if command == 'PEGA' or command == 'CUMP':
-                    if len(data) > 2:
-                        status = self.operations[command](conn, data[2])
-                        if not status: break
+                if command == 'PEGA' or command == 'CUMP' and len(data) > 2:
+                    status = self.operations[command](conn, data[2])
+                    if not status: break
 
                 elif command == 'LIST':
                     self.operations[command](conn)
